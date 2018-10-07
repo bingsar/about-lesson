862a075ab8c2e924e9f4432761aec6302b0a448b
Anton Shmigelskiy <tony.shmig@gmail.com>
2018-10-07 23:22:22 +0300

    changes

--- randomNumber.php
+++ randomNumber.php
@@ -2,18 +2,31 @@
 $x = rand(0,100);
 $a = 1;
 $b = 1;
+?>
 
-while (true) {
-    if ($a > $x) {
-        echo "Задуманное число $x НЕ входит в числовой ряд";
-        break;
-    } elseif ($a == $x) {
-        echo "Задуманное число $x входит в числовой ряд";
-        break;
-    }
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+while ($a<$x) {
     $c = $a;
     $a = $a + $b;
     $b = $c;
-
+    }
+if ($a > $x) {
+echo "Задуманное число $x НЕ входит в числовой ряд";
+} elseif ($a == $x) {
+echo "Задуманное число $x входит в числовой ряд";
 }
+
 ?>
+
+</body>
+</html>
+
